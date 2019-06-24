89841deba5c3c6dc8dda12fd5afbf3baa864c540
Defaf <defaf_kamfar@hotmail.com>
2019-06-24 11:00:20 +0300

    Finish solving warmup_week_07_day_02

--- main.js
+++ main.js
@@ -6,8 +6,8 @@ let numsSquared = [];
 // Use .map() to get the square of nums and store the value in numsSquared
 // numsSquared => [1, 4, 9, 16, 25]
 // YOUR CODE HERE //
-
-
+numsSquared = nums.map(number => number *number)
+console.log(numsSquared);
 
 // 2 isDivisibleBy3
 let nums = [1, 2, 3, 4, 5, 6, 7, 8, 9];
@@ -17,8 +17,8 @@ let isDivisibleBy3 = [];
 // isDivisibleBy3 => [3, 6, 9]
 
 // YOUR CODE HERE //
-
-
+isDivisibleBy3 = nums.filter(num => num % 3 === 0)
+console.log(isDivisibleBy3);
 
 
 // 3 Abbreviations
@@ -29,8 +29,8 @@ let dayAbbreviations = [];
 // dayAbbreviations => ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
 // YOUR CODE HERE //
 
-
-
+dayAbbreviations = days.map(day => day.slice(0,3))
+console.log(dayAbbreviations)
 
 // 4 Capitalize
 const iAS = ['ahmed', 'sami', 'moath', 'ghadeer'];
@@ -40,8 +40,8 @@ let capitalizedIAs = [];
 // capitalizedIAs => ["AHMED", "SAMI", "MOATH", "GHADEER"]
 // YOUR CODE HERE //
 
-
-
+capitalizedIAs = iAS.map( name => name.toUpperCase())
+console.log(capitalizedIAs)
 
 
 // 5 Crazy numbers, again!
@@ -52,8 +52,14 @@ let totalNumbersUnder4000 = 0;
 // Convert numbers from strings to numbers and sum all numbers under 4000 and store them in totalNumbersUnder4000
 // totalNumbersUnder4000 => 3802.4078926536
 // YOUR CODE HERE //
-
-
+let changeToNumber = stringNumbers.map(num => parseInt(num))
+totalNumbersUnder4000 = changeToNumber.reduce((num1,num2) => {
+if(num2 < 4000){
+    num1 += num2
+}
+return num1
+},0);
+console.log(totalNumbersUnder4000);
 
 
 
